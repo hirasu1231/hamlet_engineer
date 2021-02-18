864ef26613467408bea1fa929a1002e3b3d3c8f0
hirasu1231 <hirasu1231@gmail.com>
2021-02-18 13:49:40 +0000

    20210218 7th UPDATE

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -34,9 +34,8 @@ module.exports = {
             // titleは検索時に表示されるサイトのタイトル
             title: ($page, $site) => $page.title || $site.title,
             // imageはSNSでリンクした際などにプレビュー表示される画像
-            author: (_, $site) => $site.themeConfig.author,
-            twitterCard: _ => 'summary_large_image',
-            // image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image) || 'https://placehold.jp/40//fff/600x315.png?css=%7B"padding"%3A"%200%2080px"%2C"background-image"%3A"%20url(https%3A%2F%2Fplacehold.jp%2F057%2F333%2F130x40.png%3Ftext%3Dhamlet_engineer)"%7D&text='+encodeURIComponent($page.title||$site.title),
+            // twitterCard: _ => 'summary_large_image',
+            image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image) || 'https://placehold.jp/40//fff/600x315.png?css=%7B"padding"%3A"%200%2080px"%2C"background-image"%3A"%20url(https%3A%2F%2Fplacehold.jp%2F057%2F333%2F130x40.png%3Ftext%3Dhamlet_engineer)"%7D&text='+encodeURIComponent($page.title||$site.title),
         },
       },
       
